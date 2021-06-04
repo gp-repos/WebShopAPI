41cbdde0c3dc243d68db124c0f0f2938368149f1
Pavel G <3k9edccittp6@mail.ru>
2021-06-04 15:53:35 +0300

    Added Domain Entities

---
+++ WebShop.Core/Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebShop.Core.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Excerpt { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string ImageFile { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+        
+        public Category Category { get; set; }
+
+    }
+}
