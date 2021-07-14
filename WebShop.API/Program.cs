3f1473542a72d418a3f2f9f4bf971ea72ec6aa4d
Pavel G <3k9edccittp6@mail.ru>
2021-07-14 18:44:48 +0300

    Fix Serilog usage error

--- WebShop.API/Program.cs
+++ WebShop.API/Program.cs
@@ -34,6 +34,7 @@ namespace WebShop.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
